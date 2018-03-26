d625637a17a8eda413e02f05672ce3936e3a2f2e
NoyaFangzhou <noya_fangzhou@sina.com>
2018-03-26 13:59:06 -0400

    Add files use in csug

---
+++ cs255-llvm-idx/cs255-llvm/lib/Debug+Asserts/InstCounter.d
@@ -0,0 +1,308 @@
+/home/hoover/u1/cs255/cs255-llvm/lib/Debug+Asserts/InstCounter.o \
+ /home/hoover/u1/cs255/cs255-llvm/lib/Debug+Asserts/InstCounter.d: \
+ InstCounter.cpp /u/cs255/dc_llvm/llvm/include/llvm/ADT/Statistic.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/Atomic.h \
+ /u/cs255/dc_llvm/build/include/llvm/Support/DataTypes.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/Compiler.h \
+ /u/cs255/dc_llvm/build/include/llvm/Config/llvm-config.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Function.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/ilist_node.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/ilist_node_base.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/PointerIntPair.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/PointerLikeTypeTraits.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/ilist_node_options.h \
+ /u/cs255/dc_llvm/build/include/llvm/Config/abi-breaking.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/iterator_range.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/StringRef.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/STLExtras.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/Optional.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/None.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/AlignOf.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/iterator.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/ErrorHandling.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Argument.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/Twine.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/SmallVector.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/MathExtras.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/SwapByteOrder.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/type_traits.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Attributes.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/ArrayRef.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/Hashing.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/Host.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/StringMap.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/Allocator.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/FoldingSet.h \
+ /u/cs255/dc_llvm/llvm/include/llvm-c/Types.h \
+ /u/cs255/dc_llvm/build/include/llvm/IR/Attributes.gen \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Value.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Use.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/CBindingWrapping.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/Casting.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Value.def \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/BasicBlock.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/ilist.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/simple_ilist.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/ilist_base.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/ilist_iterator.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Instruction.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/DebugLoc.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/TrackingMDRef.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Metadata.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/DenseMap.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/DenseMapInfo.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/EpochTracker.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/PointerUnion.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Constant.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/User.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/LLVMContext.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/Options.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/CommandLine.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/SmallPtrSet.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/ManagedStatic.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Metadata.def \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/SymbolTableListTraits.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Instruction.def \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/CallingConv.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/GlobalObject.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/GlobalValue.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/DerivedTypes.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Type.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/APFloat.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/APInt.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/MD5.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/ADT/SmallString.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/Endian.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Intrinsics.h \
+ /u/cs255/dc_llvm/build/include/llvm/IR/Intrinsics.gen \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/OperandTraits.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Module.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Comdat.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/DataLayout.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Pass.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/PassSupport.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Pass.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/InitializePasses.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/PassInfo.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/PassRegistry.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/RWMutex.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/Threading.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/PassAnalysisSupport.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/GlobalAlias.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/GlobalIndirectSymbol.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/GlobalIFunc.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/GlobalVariable.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/CodeGen.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/IRBuilder.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/ConstantFolder.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Constants.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/InstrTypes.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Instructions.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/AtomicOrdering.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/Operator.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/IR/ValueHandle.h \
+ /u/cs255/dc_llvm/llvm/include/llvm/Support/raw_ostream.h
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/Statistic.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/Atomic.h:
+
+/u/cs255/dc_llvm/build/include/llvm/Support/DataTypes.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/Compiler.h:
+
+/u/cs255/dc_llvm/build/include/llvm/Config/llvm-config.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Function.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/ilist_node.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/ilist_node_base.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/PointerIntPair.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/PointerLikeTypeTraits.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/ilist_node_options.h:
+
+/u/cs255/dc_llvm/build/include/llvm/Config/abi-breaking.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/iterator_range.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/StringRef.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/STLExtras.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/Optional.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/None.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/AlignOf.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/iterator.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/ErrorHandling.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Argument.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/Twine.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/SmallVector.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/MathExtras.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/SwapByteOrder.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/type_traits.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Attributes.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/ArrayRef.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/Hashing.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/Host.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/StringMap.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/Allocator.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/FoldingSet.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm-c/Types.h:
+
+/u/cs255/dc_llvm/build/include/llvm/IR/Attributes.gen:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Value.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Use.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/CBindingWrapping.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/Casting.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Value.def:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/BasicBlock.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/ilist.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/simple_ilist.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/ilist_base.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/ilist_iterator.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Instruction.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/DebugLoc.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/TrackingMDRef.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Metadata.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/DenseMap.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/DenseMapInfo.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/EpochTracker.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/PointerUnion.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Constant.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/User.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/LLVMContext.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/Options.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/CommandLine.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/SmallPtrSet.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/ManagedStatic.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Metadata.def:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/SymbolTableListTraits.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Instruction.def:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/CallingConv.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/GlobalObject.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/GlobalValue.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/DerivedTypes.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Type.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/APFloat.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/APInt.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/MD5.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/ADT/SmallString.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/Endian.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Intrinsics.h:
+
+/u/cs255/dc_llvm/build/include/llvm/IR/Intrinsics.gen:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/OperandTraits.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Module.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Comdat.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/DataLayout.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Pass.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/PassSupport.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Pass.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/InitializePasses.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/PassInfo.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/PassRegistry.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/RWMutex.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/Threading.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/PassAnalysisSupport.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/GlobalAlias.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/GlobalIndirectSymbol.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/GlobalIFunc.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/GlobalVariable.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/CodeGen.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/IRBuilder.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/ConstantFolder.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Constants.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/InstrTypes.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Instructions.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/AtomicOrdering.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/Operator.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/IR/ValueHandle.h:
+
+/u/cs255/dc_llvm/llvm/include/llvm/Support/raw_ostream.h:
---
+++ cs255-llvm-idx/cs255-llvm/runtime/Debug+Asserts/InstCounting.d
@@ -0,0 +1,3 @@
+/home/hoover/u1/cs255/cs255-llvm/runtime/Debug+Asserts/InstCounting.o \
+ /home/hoover/u1/cs255/cs255-llvm/runtime/Debug+Asserts/InstCounting.d: \
+ InstCounting.c
