2c39d39b1217479c69d554af29e486b7955803ee
NoyaFangzhou <noya_fangzhou@sina.com>
2018-04-22 13:58:55 -0400

    update the test case suite

--- cs255-llvm-idx/test/test.c
+++ cs255-llvm-idx/test/test.c
@@ -1,24 +1,37 @@
-#include <stdio.h>
-#include <string.h>
- 
-int main() {
-   char str[100], temp;
-   int i, j = 0;
- 
-   printf("\nEnter the string :");
-   gets(str);
- 
-   i = 0;
-   j = strlen(str) - 1;
- 
-   while (i < j) {
-      temp = str[i];
-      str[i] = str[j];
-      str[j] = temp;
-      i++;
-      j--;
-   }
- 
-   printf("\nReverse string is :%s", str);
-   return (0);
+void
+test()
+{
+	//  int *a, int* b, int* c, int* d, int * e, int *f, int *g
+	int a[100], b[100], c[100], d[100], e[100], f[100], g[100];
+	// test for different reference
+	for (int i = 1; i < 100; ++i)
+	{
+		a[i+1] = b[i] + 1;
+	}
+
+	// test for wz-siv
+	for (int i = 1; i < 100; ++i)
+	{
+		c[i] = c[1] + c[100];
+	}
+
+	// test for default
+	for (int i = 1; i < 100; ++i)
+	{
+		d[i] = d[i] + d[i-1];
+	}
+	// test for wc-siv
+	for (int i = 1; i < 100; ++i)
+	{
+		e[i] = e[100-i];
+	}
+
+	// test for wc-siv
+	for (int i = 1; i < 100; ++i)
+	{
+		f[i] = f[100-i+1];
+	}
+	for (int i = 1; i < 100; i++) {
+		g[3] = g[5] + 1;
+	}
 }
